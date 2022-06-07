ea05a47578798a5b6686ab8fc51d6666c941fb50
Paulina Zawadzka <p.zawadzka@livechat.com>
2022-06-07 21:58:31 +0200

    Adding cypress tests

--- cypress/integration/ticket-form.spec.ts
+++ cypress/integration/ticket-form.spec.ts
@@ -1,1 +1,35 @@
 // Your tests go in here. Happy coding! 🤓
+describe('Ticket form', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+      cy.get('input#name').type('Paulina')
+      cy.get('input#email').type('paulina.zawadzka1412@gmail.com')
+      cy.get('input#subject').type('my subject')
+      cy.get('textarea#message').type('This is my first Cypress test')
+    })
+      it('Successful form submission', () => { 
+          cy.intercept('POST', 'https://api.livechatinc.com/v2/tickets/new', (req) => {
+              req.reply({
+                statusCode: 200,
+              })
+            }).as('submitForm')
+            cy.get('form').submit()
+            cy.wait('@submitForm')
+              .its('response.statusCode').should('eq', 200)
+            cy.contains('Thank you')
+      })
+      it('Unsuccessful form submission', () => {
+         cy.intercept('POST', 'https://api.livechatinc.com/v2/tickets/new', (req) => {
+             req.reply({
+               statusCode: 500
+             })
+           }).as('submitForm')
+           cy.get('form').submit()
+           cy.wait('@submitForm')
+              .its('response.statusCode').should('eq', 500)
+           cy.contains('Error')
+     }) 
+  })
+  
+  
+  
